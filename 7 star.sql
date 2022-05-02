2034ca2ceee764ef3b89ee01e8bb78e799c5a1a0
vgy12 <vgy_12@hotmail.com>
2022-05-02 15:51:47 +0400

    driver details and logout

--- 7 star.sql
+++ 7 star.sql
@@ -7,3 +7,16 @@ select * from users;
 select max(id) as id from users;
 truncate table users;
 alter table users change id id INT(40) AUTO_INCREMENT;
+
+
+CREATE TABLE `seven_stars`.`driver_details` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `emid` VARCHAR(255) NOT NULL,
+  `licno` VARCHAR(255) NOT NULL,
+  `emidvald` VARCHAR(255) NOT NULL,
+  `licsdate` DATE NOT NULL,
+  `licedate` DATE NOT NULL,
+  `passportno` VARCHAR(255) NOT NULL,
+  `validity` VARCHAR(255) NOT NULL,
+  `sponsor` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`));
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, url_for, jsonify, make_response, flash
+import flask
+from flask import Flask, render_template, request, url_for, jsonify, make_response, flash,session
 from flask_mysqldb import MySQL
 import hashlib
 from flask_login import login_manager, UserMixin, login_user, login_required, logout_user, current_user
@@ -13,7 +14,7 @@ app.config['SECRET_KEY'] = "This is my secret key"
 # app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/seven_stars'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Prince1988'
 app.config['MYSQL_DB'] = 'seven_stars'
 mysql = MySQL(app)
 
@@ -24,13 +25,19 @@ mysql = MySQL(app)
 # Page not found
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    if (session.get('empno') != None):
+        return render_template("404.html"), 404
+    else:
+        return flask.redirect('/')
 
 
 # Server error page
 @app.errorhandler(500)
 def page_not_found(e):
-    return render_template("500.html"), 500
+    if (session.get('empno') != None):
+        return render_template("500.html"), 500
+    else:
+        return flask.redirect('/')
 
 
 # home page
@@ -42,110 +49,175 @@ def home():
 # Rental Agreement Form
 @app.route('/rentalAgree', methods=['POST', 'GET'])
 def rentalAgreemnet():
-    return render_template('rental_agreement_form.html')
+    if (session.get('empno') != None):
+        return render_template('rental_agreement_form.html')
+    else:
+        return flask.redirect('/')
 
 
 @app.route('/getvehicle', methods=['POST', 'GET'])
 def getVehicleType():
-    # Search String
-    queryString = request.form['queryString']
+    if (session.get('empno') != None):
 
-    # print(queryString)
+        # Search String
+        queryString = request.form['queryString']
 
-    # Creating a connection cursor
-    cursor = mysql.connection.cursor()
-    cursor.execute('''select * from vehicletype WHERE vehtype LIKE '{}%' LIMIT 5 '''.format(queryString))
-    vehdata = list(cursor.fetchall())
-    vehlist = []
+        # print(queryString)
 
-    for vehdatas in vehdata:
-        vehlist.append(vehdatas[1])
+        # Creating a connection cursor
+        cursor = mysql.connection.cursor()
+        cursor.execute('''select * from vehicletype WHERE vehtype LIKE '{}%' LIMIT 5 '''.format(queryString))
+        vehdata = list(cursor.fetchall())
+        vehlist = []
 
-    # Saving the Actions performed on the DB
-    mysql.connection.commit()
+        for vehdatas in vehdata:
+            vehlist.append(vehdatas[1])
 
-    # Closing the cursor
-    cursor.close()
+        # Saving the Actions performed on the DB
+        mysql.connection.commit()
 
-    # print(vehlist)
-    # print(len(vehlist))
+        # Closing the cursor
+        cursor.close()
 
-    response = jsonify({'htmlresponse': render_template('response.html', vehdata=vehlist)})
+        # print(vehlist)
+        # print(len(vehlist))
 
-    return response
+        response = jsonify({'htmlresponse': render_template('response.html', vehdata=vehlist)})
 
+        return response
+
+    else:
+        return flask.redirect('/')
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
-    salt = "5gz"
-    name = request.form['name']
-    email = request.form['emailid']
-    empno = request.form['empno']
-    password = str(request.form['pass1']) + salt
-    hashedpass = hashlib.md5(password.encode())
-    db_password = hashedpass.hexdigest()
+    if (session.get('empno') != None):
 
-    print(name + "  " + email + "   " + empno + "  " + db_password)
+        salt = "5gz"
+        name = request.form['name']
+        email = request.form['emailid']
+        empno = request.form['empno']
+        password = str(request.form['pass1']) + salt
+        hashedpass = hashlib.md5(password.encode())
+        db_password = hashedpass.hexdigest()
 
-    # Creating a connection cursor
-    cursor = mysql.connection.cursor()
-    cursor.execute('''select max(id) as id from users ''')
+        # print(name + "  " + email + "   " + empno + "  " + db_password)
 
-    id = cursor.fetchone()
+        # Creating a connection cursor
+        cursor = mysql.connection.cursor()
+        cursor.execute('''select max(id) as id from users ''')
 
-    if(id[0] is None):
-        cursor.execute('''insert into users (id,name,empno,email,password,createdon) values(%s,%s,%s,%s,%s,now())''',
-                       (1001, name, empno, email, db_password))
+        id = cursor.fetchone()
 
+        if(id[0] is None):
+            cursor.execute('''insert into users (id,name,empno,email,password,createdon) values(%s,%s,%s,%s,%s,now())''',
+                           (1001, name, empno, email, db_password))
+
+        else:
+            cursor.execute('''insert into users (name,empno,email,password,createdon) values(%s,%s,%s,%s,now())''', (name,
+                                                                                                                     empno,
+                                                                                                                     email,
+                                                                                                                     db_password))
+
+        # Saving the Actions performed on the DB
+        mysql.connection.commit()
+
+        # Closing the cursor
+        cursor.close()
+
+        flash("User Created Successfully")
+
+        return render_template("login.html")
     else:
-        cursor.execute('''insert into users (name,empno,email,password,createdon) values(%s,%s,%s,%s,now())''', (name,
-                                                                                                                 empno,
-                                                                                                                 email,
-                                                                                                                 db_password))
-
-
-    # Saving the Actions performed on the DB
-    mysql.connection.commit()
-
-    # Closing the cursor
-    cursor.close()
-
-    flash("User Created Successfully")
-
-    return render_template("login.html")
-
+        return flask.redirect('/')
 
 
 @app.route('/userauth', methods=['POST','GET'])
 def userauthentication():
 
+        salt = "5gz"
+        # name = request.form['name']
+        # empno = request.form['empno']
+        empno = request.form['empnologin']
+        password = str(request.form['password']) + salt
+        hashedpass = hashlib.md5(password.encode())
+        db_password = hashedpass.hexdigest()
 
-    salt = "5gz"
-    # name = request.form['name']
-    # empno = request.form['empno']
-    email = request.form['email']
-    password = str(request.form['password']) + salt
-    hashedpass = hashlib.md5(password.encode())
-    db_password = hashedpass.hexdigest()
+        # Creating a connection cursor
+        cursor = mysql.connection.cursor()
+        cursor.execute('''select * from users where empno=%s and password=%s''',(empno,db_password))
 
-    # Creating a connection cursor
-    cursor = mysql.connection.cursor()
-    cursor.execute('''select * from users where email=%s and password=%s''',(email,db_password))
+        chk = cursor.rowcount
 
-    chk = cursor.rowcount
+        # Saving the Actions performed on the DB
+        mysql.connection.commit()
 
-    # Saving the Actions performed on the DB
-    mysql.connection.commit()
+        # Closing the cursor
+        cursor.close()
 
-    # Closing the cursor
-    cursor.close()
+        if(chk>0):
+            session['empno'] = empno
+            return render_template('home.html')
+        else:
+            flash("Warning!!!  Invalid user!!!")
+            return render_template('login.html')
 
-    if(chk>0):
-        return render_template('home.html')
+@app.route('/logout')
+def logout():
+    session.pop('empno',None)
+    return render_template('login.html')
+
+
+@app.route('/driverdetails')
+def driverdetails():
+
+    if(session.get('empno')!=None):
+        return render_template('driver_details.html')
     else:
-        flash("Warning!!!  Invalid user!!!")
-        return render_template('login.html')
+        return flask.redirect('/')
 
+
+
+@app.route('/savedriverdetails', methods=['POST', 'GET'])
+def savedriver():
+    if (session.get('empno') != None):
+
+        name = request.form['dname']
+        emid = request.form['emid']
+        licno = request.form['licno']
+        emidval = request.form['emidvalidity']
+        licsdate = request.form['licsdate']
+        licedate = request.form['licedate']
+        passno = request.form['passportno']
+        pvalidity = request.form['passportval']
+        sponname = request.form['sponsorname']
+
+
+        # Creating a connection cursor
+        cursor = mysql.connection.cursor()
+        cursor.execute('''select max(id) as id from driver_details ''')
+
+        id = cursor.fetchone()
+
+        if(id[0] is None):
+            cursor.execute('''insert into driver_details (id,name,empno,email,password,createdon) values(%s,%s,%s,%s,%s,now())''',
+                           (1001, name, ))
+        else:
+            cursor.execute('''insert into driver_details (name,empno,email,password,createdon) values(%s,%s,%s,%s,now())''', (name,
+                                                                                                                     ))
+
+
+        # Saving the Actions performed on the DB
+        mysql.connection.commit()
+
+        # Closing the cursor
+        cursor.close()
+
+        flash("User Created Successfully")
+
+        return render_template("login.html")
+    else:
+        return flask.redirect('/')
 
 
 
